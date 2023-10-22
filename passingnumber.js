c19c8bca3e8a3c959f7a0f8286b13d60e7227f23
kuldeepsrit <144715653+kuldeepsrit@users.noreply.github.com>
2023-10-22 12:59:10 +0530

    Add files via upload

---
+++ passingnumber.js
@@ -0,0 +1,16 @@
+console.log(8)
+console.log("kuldeep");
+const b = 'ram';
+console.log(b);
+
+function table() {
+    return 'hello world'
+}
+console.log(table());
+const subject = 'ram';
+console.log("helllo");
+let obj = {
+    num: "simpu"
+
+}
+console.log(obj);
